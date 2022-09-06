12602f88d6b8801557fea18116f983872b5bc63b
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-09-05 23:47:53 -0300

    Métodos da string

---
+++ aula-novo-semestre/aula-[05][9][2022]-parte4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    //string - métodos: OPERATOR[]
+    string word = "Hello";
+    cout<< word[0] << endl;
+    cout<< word[1] << endl;
+    cout<< word[2] << endl;
+    cout<< word[3] << endl;
+    cout<< word[4] << endl;
+    
+    //string - métodos: OPERATOR=
+    string word2 = "Leaving"
+    
+
+    return 0;
+}
