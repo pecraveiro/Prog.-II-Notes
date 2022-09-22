55eaba7998d0486aa0f05ff55bf2575921fabb21
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-09-22 09:05:44 -0300

    Create aula-[21][9][2022]-parte2.cpp

---
+++ aula-novo-semestre/aula-[21][9][2022]-parte2.cpp
@@ -0,0 +1,231 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+//prototypes
+void InsertWord(vector<string> &localList);
+void PrintWords(const vector<string> &localList);
+bool SearchWord(const vector<string> &localList);
+bool RemoveWordByIndex(vector<string> &localList);
+vector<size_t> SearchSubstring(const vector<string> &localList);
+bool RemoveSubstrings(vector<string> &localList);
+
+bool LoadDatabase(string filename, vector<string> &localList);
+bool SaveDatabase(string filename, const vector<string> &localList);
+
+int main()
+{
+    vector<string> ListOfWords;
+    if(LoadDatabase("database.txt", ListOfWords) == false)
+    {
+        cout << "Database file not found. Creating a new txt file. Welcome." << endl;
+    }
+    
+    while(true) //main loop
+    {
+        cout << "--------------------------------------------------" << endl;
+        cout << "UFxC String Store V.0" << endl;
+        cout << "1. Insert string" << endl;
+        cout << "2. Print index and string" << endl;
+        cout << "3. Search string (literalpos)" << endl;
+        cout << "4. Search substrings" << endl;
+        cout << "5. Remove (by index)" << endl;
+        cout << "6. Remove by substrings (all occurrences)" << endl;
+        cout << "0. Quit" << endl;
+        cout << "--------------------------------------------------" << endl << endl;
+        cout << "Select an option: ";
+
+        char ch;
+        cin >> ch;
+
+        //conditions or switch case
+        if(ch == '1')
+        {
+            InsertWord(ListOfWords);
+            continue;
+        }
+        if(ch == '2')
+        {
+            PrintWords(ListOfWords);
+            continue;
+        }
+
+        if(ch == '3')
+        {
+            SearchWord(ListOfWords);
+            continue;
+        }
+        if(ch == '4')
+        {
+            vector<size_t> indexes = SearchSubstring(ListOfWords);
+            if(indexes.size() == 0)
+                cout << "Substring not found" << endl;
+
+            continue;
+        }
+        if(ch == '5')
+        {
+            RemoveWordByIndex(ListOfWords);
+            continue;
+        }
+        if(ch == '6')
+        {
+            RemoveSubstrings(ListOfWords);
+            continue;
+        }
+        if(ch == '0')
+        {
+            if(SaveDatabase("database.txt", ListOfWords) == false)
+            {
+                cout << "Error, unable to save the database." << endl;
+            }
+            else
+                break;
+        }
+    }
+    return 0;
+}
+
+//passagem por referencia para atualizaçao / remoçao
+void InsertWord(vector<string> &localList)
+{
+    cout << "Insert a word: ";
+    string str;
+    cin >> str;
+    localList.push_back(str);
+}
+
+//passagem por referencia para consulta (sem copia)
+void PrintWords(const vector<string> &localList)
+{
+    cout << "Print list of words" << endl;
+    cout << "List of words has " << localList.size() << endl;
+
+    for(size_t i=0; i<localList.size(); i++)
+    {
+        cout << "  Index " << i << " -> " << localList.at(i) << endl;
+    }
+}
+
+bool SearchWord(const vector<string> &localList)
+{
+    cout << "Enter with a word to search: ";
+    string query;
+    cin >> query;
+
+    for(size_t i=0; i<localList.size(); i++)
+    {
+        if(localList.at(i) == query)
+        {
+            cout << "-> Word " << query << " has found at position " << i << endl;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool RemoveWordByIndex(vector<string> &localList)
+{
+    cout << "Enter with an index to remove: ";
+    int index;
+    cin >> index;
+
+    if(index >= 0 && index < localList.size())
+    {
+        localList.erase(localList.begin()+index);
+        return true;
+    }
+    else
+    {
+        cout << "Idiot !!!!, Invalid Index !" << endl;
+        return false;
+    }
+}
+
+vector<size_t> SearchSubstring(const vector<string> &localList)
+{
+    cout << "Enter with a substring to search: ";
+    string str;
+    cin >> str;
+
+    cout << "Searching for substring: " << str << endl;
+    vector<size_t> indexes;
+    for(size_t i=0; i< localList.size(); i++)
+    {
+        size_t position = localList.at(i).find(str);
+
+        if(position < localList.at(i).size())
+        {
+            indexes.push_back(i);
+            cout << " Substring has found : " << localList.at(i) << endl;
+        }
+    }
+    return indexes;
+}
+
+bool RemoveSubstrings(vector<string> &localList)
+{
+    cout << "Enter with a substring to search: ";
+    string str;
+    cin >> str;
+
+    cout << "Searching for substring to removal: " << str << endl;
+
+    bool firstremoval = false;
+    size_t i=0;
+    while(i<localList.size())
+    {
+        size_t position = localList.at(i).find(str);
+
+        if(position < localList.at(i).size())
+        {
+            cout << "Substring has found, removing it: " << localList.at(i) << endl;
+            localList.erase(localList.begin()+i);
+            firstremoval = true;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    return firstremoval;
+}
+
+bool LoadDatabase(string filename, vector<string> &localList)
+{
+    ifstream filereader(filename);
+    if(filereader.is_open())
+    {
+        string tmp;
+        while(getline(filereader, tmp))
+        {
+            localList.push_back(tmp);
+        }
+        filereader.close();
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool SaveDatabase(string filename, const vector<string> &localList)
+{
+    ofstream filewritter(filename);
+    if(filewritter.is_open())
+    {
+        for(size_t i=0; i<localList.size(); i++)
+        {
+            filewritter << localList.at(i) << endl;
+        }
+        filewritter.close();
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
